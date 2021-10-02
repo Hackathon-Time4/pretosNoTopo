ed87fc4c1b908b11ae55d4b14cefce6150a85991
riannselegar <riannselegar@gmail.com>
2021-10-02 13:02:52 -0300

    Criando os cards #3
    v0.1

---
+++ pretos-no-topo/src/components/CardGrid.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-row>
+      <div v-for="card in cardTable" :key="card.id">
+            <Card :p="card"/>
+      </div>
+    </v-row>
+    <v-btn @click="teste()">Teste</v-btn>  
+  </div>
+</template>
+
+<script>
+import Card from './Card.vue';
+
+export default {
+  components: { Card },
+  name: "CardGrid",
+  data() {
+    return {
+      cardTable: []
+    }
+  },
+  async created() {
+      const response = await fetch("https://it3zxc-default-rtdb.firebaseio.com/racismo.json");
+      const d = await response.json();
+      this.cardTable = d;
+  },
+  methods: {
+
+    teste() {
+      console.log(this.cardTable)
+    },
+    getProps(index) {
+      return this.cardTable[index];
+    },
+  }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
