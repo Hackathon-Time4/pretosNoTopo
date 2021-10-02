0446f5d77ca6945761092423e38a54152b23d483
riannselegar <riannselegar@gmail.com>
2021-10-02 17:06:07 -0300

    #6 Final updates

--- pretos-no-topo/src/views/Galeria.vue
+++ pretos-no-topo/src/views/Galeria.vue
@@ -2,46 +2,42 @@
   <v-container>
     <v-row>
       <v-col>
-      <v-card
-        elevation="24"
-        max-width="550"
-        class="mx-auto my-6"
-      >
-        <v-carousel
-          :continuous="false"
-          :cycle="cycle"
-          :show-arrows="true"
-          hide-delimiter-background
-          delimiter-icon="mdi-minus"
-          height="450"
-        >
-          <v-carousel-item
-            v-for="(image, i) in imagens"
-            :key="i"
-            :src="image.src"
+        <v-card elevation="24" max-width="550" class="mx-auto my-6">
+          <v-carousel
+            :continuous="false"
+            :cycle="cycle"
+            :show-arrows="true"
+            hide-delimiter-background
+            delimiter-icon="mdi-minus"
+            height="450"
           >
-            <v-sheet>
-              <v-row class="fill-height" align="center" justify="center">
-              </v-row>
-            </v-sheet>
-          </v-carousel-item>
-        </v-carousel>
-        <v-list two-line>
-          <v-list-item>
-            <v-list-item-avatar>
-              <v-img src="../assets/instagram.png"></v-img>
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <a
-                class="instagram"
-                href="https://www.instagram.com/projetopretosnotopo/"
-                target="_blank"
-                >@projetopretosnotopo</a
-              >
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-card>
+            <v-carousel-item
+              v-for="(image, i) in imagens"
+              :key="i"
+              :src="image.src"
+            >
+              <v-sheet>
+                <v-row class="fill-height" align="center" justify="center">
+                </v-row>
+              </v-sheet>
+            </v-carousel-item>
+          </v-carousel>
+          <v-list two-line>
+            <v-list-item>
+              <v-list-item-avatar>
+                <v-img src="../assets/instagram.png"></v-img>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <a
+                  class="instagram"
+                  href="https://www.instagram.com/projetopretosnotopo/"
+                  target="_blank"
+                  >@projetopretosnotopo</a
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card>
       </v-col>
       <v-col class="my-6">
         <iframe
@@ -62,7 +58,7 @@ export default {
   name: "Galeria",
   data() {
     return {
-      cycle: false,
+      cycle: true,
       imagens: [
         {
           src: require("../../public/images/familia.jpg"),
--- pretos-no-topo/src/views/Inicio.vue
+++ pretos-no-topo/src/views/Inicio.vue
@@ -1,63 +1,93 @@
 <template>
-    <v-container fluid d-flex flex-column  align-center class="container">
-         <v-img class="image"
-      lazy-src="../../public/images/bg-2.png"
-  max-width="250"
-      src="../../public/images/bg-2.png"
-      >
-         </v-img>
-    <div id="flex">
-      <v-text class="text-sm-body-3 px-5 text">
-        Olá, bem vindos(as) ao Pretos No Topo.
-        Esse projeto foi criado com a intenção de enaltecer a cultura preta.
-        E como fazemos isso? Com muita informação!
-        Aqui você poderá encontrar algumas potências negras, livros, artigos, videos e muito mais!
-        <blockquote class="sidekick mt-4">
-        Eu tenho um sonho. O sonho de ver meus filhos julgados por sua personalidade, não pela cor de sua pele. <cite> (Martin Luther King)</cite>
-        </blockquote>
-      </v-text>
-    
-      <v-img class="image"
-      lazy-src="../../public/images/bg.png"
-      src="../../public/images/bg.png"
+  <v-container fluid d-flex flex-column align-center class="container">
+    <v-row>
+      <v-img
+        class="image"
+        lazy-src="../../public/images/bg-2.png"
+        max-width="250"
+        src="../../public/images/bg-2.png"
       >
       </v-img>
-    </div>
+    </v-row>
+    <v-row>
+      <div id="flex">
+        <v-text class="text-sm-body-3 px-5 text">
+          Olá, bem vindos(as) ao Pretos No Topo. Esse projeto foi criado com a
+          intenção de enaltecer a cultura preta. E como fazemos isso? Com muita
+          informação! Aqui você poderá encontrar algumas potências negras,
+          livros, artigos, videos e muito mais!
+          <blockquote class="sidekick mt-4">
+            Eu tenho um sonho. O sonho de ver meus filhos julgados por sua
+            personalidade, não pela cor de sua pele.
+            <cite> (Martin Luther King)</cite>
+          </blockquote>
+        </v-text>
 
-    </v-container>
+        <v-img
+          class="image"
+          lazy-src="../../public/images/bg.png"
+          src="../../public/images/bg.png"
+        >
+        </v-img>
+      </div>
+    </v-row>
+    <v-row class="justify-center py-2">
+      <div class="video-wrapper">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/n7DcbOpKUw8"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'Inicio'
-}
+  name: "Inicio",
+};
 </script>
 
 <style scoped>
-
 #flex {
   display: flex;
   justify-content: center;
 }
 
 .sidekick {
-position: relative;
-padding-left: 1em;
-border-left: 0.2em solid brown;
-font-family: 'Roboto', serif;
-font-size: 1.5em;
-line-height: 1.3em;
-font-weight: 100;
+  position: relative;
+  padding-left: 1em;
+  border-left: 0.2em solid brown;
+  font-family: "Roboto", serif;
+  font-size: 1.5em;
+  line-height: 1.3em;
+  font-weight: 100;
 }
-.sidekick:before, .sidekick:after {
-font-family: Calibri;
-color: brown;
-font-size: 34px;
+.sidekick:before,
+.sidekick:after {
+  font-family: Calibri;
+  color: brown;
+  font-size: 34px;
 }
-.sidekick:before {content: '\201e'}
-.sidekick:after {content: '\201c';}
-.sidekick cite {font-size: 50%; text-align:center; top:50%}
-.sidekick cite:before {content: ' \2015 '}
+.sidekick:before {
+  content: "\201e";
+}
+.sidekick:after {
+  content: "\201c";
+}
+.sidekick cite {
+  font-size: 50%;
+  text-align: center;
+  top: 50%;
+}
+.sidekick cite:before {
+  content: " \2015 ";
+}
 .image {
   max-width: 100%;
   width: 50vw;
@@ -67,16 +97,15 @@ font-size: 34px;
 .text {
   max-width: 90%;
   font-size: 25px;
-  font-family: 'Roboto', serif;
+  font-family: "Roboto", serif;
 }
 
 @media (max-width: 768px) {
   .image {
-  width: 90vw;
-}
+    width: 90vw;
+  }
   #flex {
     flex-direction: column-reverse;
   }
 }
-
 </style>
